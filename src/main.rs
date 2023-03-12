4d41ae931a67ec8fb766b4522f7a589b9614b703
Ben Wiklund <ben@daisyowl.com>
2023-03-12 14:17:23 -0700

    setup

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,19 @@ fn encode(path: &str) {
     let img = image::open(path).unwrap();
 
     // print disk size of the path
+    let raw_size_bytes = img.width() * img.height() * 4;
+    println!(
+        "{} uncompress -> {} bytes",
+        path,
+        raw_size_bytes
+    );
+
     let metadata = std::fs::metadata(path).unwrap();
     let size = metadata.len();
-    println!("{} is {} bytes", path, size);
+    println!(
+        "{} as png -> {} bytes ({}%)",
+        path,
+        size,
+        100.0 * (img.width() * img.height()) as f32 / raw_size_bytes as f32
+    );
 }
